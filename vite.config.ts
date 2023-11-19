b0e151e201ed954f8e36b80d7d43d9c7ed4e3112
Alex <grashupfer99@gmail.com>
2023-11-20 00:19:00 +0900

    update configuration

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://grashupfer99.github.io/random-taste-app",
   plugins: [react(), tsconfigPaths()],
   server: {
     host: "0.0.0.0",
